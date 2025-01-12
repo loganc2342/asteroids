ae1a3c9a1b3892b5722922be2cca64600718429c
loganc2342 <loganc2342@gmail.com>
2025-01-12 11:59:54 -0800

    allow bullets to destroy asteroids and limit rate of fire

--- main.py
+++ main.py
@@ -43,6 +43,10 @@ def main():
             if asteroid.check_collision(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.check_collision(asteroid):
+                    shot.kill()
+                    asteroid.kill()
 
         screen.fill("black")
 
